7b141564dbf981615f662fc90138ef0d513136c4
JLezzzzz <teeramaet.w@gmail.com>
2025-04-02 15:50:22 +0700

    feat(file): Added post & delete method to the admin component.

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -16,22 +16,68 @@ export default function Admin() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                let members = await axios.get("https://jsd5-mock-backend.onrender.com/members")
-                setData(members.data)
-                console.log(data)
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [position, setPosition] = useState("");
 
-            } catch (err) {
-                setError ("Failed to retrieve the data")
-                console.log(err)
-            } finally {
-                setLoading(false)
-            }
+
+    const fetchUsers = async () => {
+        try {
+            let members = await axios.get("https://jsd5-mock-backend.onrender.com/members")
+            setData(members.data)
+            console.log(data)
+
+        } catch (err) {
+            setError ("Failed to retrieve the data")
+            console.log(err)
+        } finally {
+            setLoading(false)
         }
+    }
+
+    const clearStuff = () => {
+        setFirstName("")
+        setLastName("")
+        setPosition("")
+    }
+    const createUser = async (e) => {
+        try {
+            e.preventDefault()
+            let payload = {name: firstName, lastname: lastName, position: position}
+            if (payload.name === "" && payload.lastname === "" && payload.position === "") {
+                alert ("Do not put an empty value.")
+            }
+            let newUser = await axios.post("https://jsd5-mock-backend.onrender.com/members",
+                payload)
+            console.log(newUser)
+        } catch(err) {
+            setError ("Eieie")
+            console.log(err)
+
+        } finally {
+            fetchUsers();
+            clearStuff();
+        }
+    }
+
+    const deleteUser = async (targetId) => {
+        try {
+            let deleted = await axios.delete(`https://jsd5-mock-backend.onrender.com/member/${targetId}`)
+            console.log(deleted)
+        } catch(err) {
+            setError("Error delete")
+            console.log(err)
+        } finally {
+            fetchUsers()
+        }
+
+    }
+
+    useEffect(() => {
         fetchUsers()
     }, [])
+
+
 
     return (
     <>
@@ -51,25 +97,26 @@ export default function Admin() {
         <div className="flex flex-col items-center">
             <div className="mb-12 w-[1077px] h-[100px]">
                 <span className="px-4 text-2xl text-black rounded-md font-bold">Create User Here</span>
-                <Form method="post" className="flex gap-12 mt-4 justify-center">
+                <Form onSubmit={createUser} method="post" className="flex gap-12 mt-4 justify-center">
                 <input
-                    name="firstName"
+                    onChange={(e) => setFirstName(e.target.value)}
+                    value={firstName}
                     type="text"
                     className="p-2 bg-gray-100 rounded-md w-[300px] h-[56px] text-black "
                     placeholder="Name"/>
                 <input
-                    name="lastName"
+                    onChange={(e) => setLastName(e.target.value)}
+                    value={lastName}
                     type="text"
                     className="p-2 bg-gray-100 rounded-md w-[300px] h-[56px] text-black "
                     placeholder="Last Name"/>
                 <input
-                    name="position"
+                    onChange={(e) => setPosition(e.target.value)}
+                    value={position}
                     type="text"
                     className="p-2 bg-gray-100 rounded-md w-[300px] h-[56px] text-black "
                     placeholder="Position"/>
-
-                    <button className="bg-[#5E5BFF] w-[63px] h-[48px] rounded-md text-white font-semibold hover:font-extrabold hover:cursor-pointer">Save</button>
-
+                    <button type="submit" className="bg-[#5E5BFF] w-[63px] h-[48px] rounded-md text-white font-semibold hover:font-extrabold hover:cursor-pointer">Save</button>
                 </Form>
             </div>
 
@@ -85,10 +132,11 @@ export default function Admin() {
                     <td className="w-[200px] border-x-1 border-y-1 text-center bg-white">{member.name}</td>
                     <td className="w-[200px] border-x-1 border-y-1 text-center bg-white">{member.lastname}</td>
                     <td className="w-[200px] border-x-1 border-y-1 text-center bg-white">{member.position}</td>
-                    <td className="w-[200px] border-x-1 border-y-1 text-center bg-white"><button className="bg-red-400 text-white px-2 w-[128px] h-[26px] rounded hover:font-bold hover:bg-red-500 hover:cursor-pointer">Delete</button></td>
+                    <td className="w-[200px] border-x-1 border-y-1 text-center bg-white"><button onClick={ ()=> deleteUser(member.id)} className="bg-red-400 text-white px-2 w-[128px] h-[26px] rounded hover:font-bold hover:bg-red-500 hover:cursor-pointer">Delete</button></td>
                 </tr>
                 ))}
             </table>
+            <button className="bg-blue-400 text-black font-bold mt-8 w-[800px] h-[32px] rounded hover:font-bold hover:bg-blue-500 hover:cursor-pointer">Edit member information</button>
             {loading && <p className="text-center bg-white mt-4">Fetching data...</p>}
             {error && <p className="text-center text-red-500">{error}</p>}
         </div>
